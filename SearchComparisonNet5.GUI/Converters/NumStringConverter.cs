a381f9af7c1c4b18d3a9ac4963bc4ca130d9f67c
Ramin Anvar <ramin.anvar@ecitcapstone.no>
2021-07-21 14:42:58 +0200

    Added validation of input parameters to MainViewModel
    Fixed a bug where CancelCommand wouldn't activate and deactivate correctly

--- SearchComparisonNet5.GUI/Converters/NumStringConverter.cs
+++ SearchComparisonNet5.GUI/Converters/NumStringConverter.cs
@@ -1,7 +1,5 @@
-﻿using SearchComparisonNet5.Kernel.Properties;
-using System;
+﻿using System;
 using System.Globalization;
-using System.Windows;
 using System.Windows.Data;
 
 namespace SearchComparisonNet5.GUI.Converters
@@ -15,38 +13,50 @@ namespace SearchComparisonNet5.GUI.Converters
 
             if (value == null)
             { return null; }
-
             var type = value.GetType();
-            if (type == typeof(double))
+
+            var isInt = long.TryParse(value.ToString(), out var intValue);
+            if (isInt)
             {
-                var val = (double)value;
-                return isAbsLogSmall
-                    ? val.ToString("G3", CultureInfo.InvariantCulture)
-                    : val.ToString("0.00E+00", CultureInfo.InvariantCulture);
+                return isAbsLogSmall ?
+                    intValue.ToString("D3", CultureInfo.InvariantCulture) :
+                    intValue.ToString("G3", CultureInfo.InvariantCulture);
             }
 
-            if (type == typeof(long))
+            var isDouble = double.TryParse(value.ToString(), out var dbValue);
+            if (isDouble)
             {
-                var val = (long)value;
                 return isAbsLogSmall
-                    ? val.ToString("G3", CultureInfo.InvariantCulture)
-                    : val.ToString("0E+00", CultureInfo.InvariantCulture);
+                    ? dbValue.ToString("G3", CultureInfo.InvariantCulture)
+                    : dbValue.ToString("0.00E+00", CultureInfo.InvariantCulture);
             }
 
-            var intValue = (int)value;
-            return isAbsLogSmall ?
-                intValue.ToString("D3", CultureInfo.InvariantCulture) :
-                intValue.ToString("G3", CultureInfo.InvariantCulture);
+            var isLong = long.TryParse(value.ToString(), out var lngValue);
+            if (isLong)
+            {
+                return isAbsLogSmall
+                    ? lngValue.ToString("G3", CultureInfo.InvariantCulture)
+                    : lngValue.ToString("0E+00", CultureInfo.InvariantCulture);
+            }
+
+            // Must be a string
+            return value.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var str = value as string;
-            if (!double.TryParse(str, out var result))
-                MessageBox.Show(Resources.InvalidIntegerError);
+            var valueStr = value as string;
+            if (int.TryParse(valueStr, out var resInt))
+                return resInt;
 
-            return result;
+            if (double.TryParse(valueStr, out var resDb))
+                return resDb;
+
+            if (long.TryParse(valueStr, out var resLng))
+                return resLng;
+
+            // Must be a string
+            return valueStr;
         }
     }
-
 }
